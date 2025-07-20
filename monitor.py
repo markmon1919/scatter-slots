914ad82cd3046436e8a45c2b0eb2e0a54974282e
Mark Mon Monteros <monteros.markmon@gmail.com>
2025-07-20 10:52:13 +0800

    optimize

--- monitor.py
+++ monitor.py
@@ -938,7 +938,7 @@ def bet_switch(bet_level: str=None, extra_bet: bool=None, slot_position: str=Non
                     pyautogui.moveTo(x=cx-100, y=cy-126)
             else:
                 if extra_bet and game.startswith("Fortune Gems"):
-                    pyautogui.moveTo(x=cx-428, y=cy+126)
+                    pyautogui.moveTo(x=cx - 550, y=cy + 200)
 
             alert_queue.put("extra_bet") if extra_bet else None
         except Empty:
