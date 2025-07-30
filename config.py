ba5b2dbaf9e0c0d088f53ca4959eee2704ee3afc
Mark Mon Monteros <monteros.markmon@gmail.com>
2025-07-30 21:52:32 +0800

    add high delta

--- config.py
+++ config.py
@@ -11,8 +11,7 @@ from collections import namedtuple
 #     return today if not countdown else timer_start
 
 LOG_LEVEL = ""#DEBUG"
-# BREAKOUT_FILE = "breakout_data.json"
-BREAKOUT_FILE = "breakout_data_test.json"
+BREAKOUT_FILE = "breakout_data.json"
 DATA_FILE = "previous_data.json"
 
 # === Screen Dimensions ===
