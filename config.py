7a31104514a45f8ece60f6a533f2723047a86ac5
Mark Mon Monteros <monteros.markmon@gmail.com>
2025-08-25 21:28:17 +0800

    add trend check

--- config.py
+++ config.py
@@ -222,7 +222,7 @@ PROVIDERS = {
     "MEGA": ProviderProps("MEGA Entertainment", WHTE, "40d155acff304e642252f253993b126e"),
     "DNA": ProviderProps("DNA Slot", WHTE, "a035b23795134651bcd27a923b7453f9"),
     "WM": ProviderProps("World Match", WHTE, "f6823427a35f41b1a378ad050a676db8"),
-    "F": ProviderProps("5", WHTE, "014e6a00b1ff714d5e5cd44064d16f48"),
+    "FIVE": ProviderProps("5", WHTE, "014e6a00b1ff714d5e5cd44064d16f48"),
     "NEXT": ProviderProps("Next Spin", WHTE, "6ad041e11133876eda65b5f993a66d4f"),
     "RG": ProviderProps("Rich Game", WHTE, "133480bdccbc68ca1bc4cea932a0e8b7"),
     "MAHA": ProviderProps("MAHA Gaming", WHTE, "fb40ff7da5ca4a8cbd1383d7d89c5b0f"),
---
+++ trend.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env .venv/bin/python
+
+import json, random, requests
+from meter import fetch_jackpot
+from config import (PROVIDERS, DEFAULT_PROVIDER_PROPS, URLS, USER_AGENTS, BLCYN, BLRED, BWHTE, DGRY, MAG, RED, YEL, WHTE, RES)
+
+# # GET TRENDING BY PROVIDER (METER)
+# curl -sG --max-time 5 "https://www.helpslot.win/api/games" -H "Accept: application/json" --data-urlencode "manuf=PG" | jq -s '[.[].data[]] | sort_by(.name)[] | select(.value >= 90) | "\(.name) \(.value)"'
+
+# # GET RED JACKPOT BY NAME (METER)
+# curl -sG --max-time 5 "https://www.helpslot.win/api/games" -H "Accept: application/json" --data-urlencode "name=Wild Ape #3258" | jq -s '[.[].data[]] | sort_by(.name)[] | select(.value >= 90) | "\(.name) \(.value)"'
+
+def render_providers():
+    providers = list(PROVIDERS.items())
+    half = (len(providers) + 1) // 2
+    lines = list()
+
+    for idx, (left_provider, left_conf) in enumerate(providers[:half], start=1):
+        left_color = left_conf.color
+        left_str = f"[{WHTE}{idx}{RES}] - {left_color}{left_conf.provider}{RES}\t"
+
+        right_index = idx - 1 + half
+        if right_index < len(providers):
+            right_provider, right_conf = providers[right_index]
+            right_color = right_conf.color
+            right_str = f"[{WHTE}{right_index + 1:>2}{RES}] - {right_color}{right_conf.provider}{RES}"
+        else:
+            right_str = ""
+
+        lines.append(f"\t{left_str:<50}\t{right_str}")
+    return "\n".join(lines)
+
+def get_game_data_from_local_api(provider: str):
+    user_agent = random.choice(USER_AGENTS)
+    REQUEST_FROM = random.choice(["H5", "H6"])
+    URL = next((url for url in URLS if 'helpslot' in url), None)
+    HEADERS = {
+        "Accept": "application/json",
+        "User-Agent": user_agent
+    }
+
+    try:
+        response = requests.get(
+            f"{URL}/api/games?manuf={provider}&requestFrom={REQUEST_FROM}",
+            headers=HEADERS
+        )
+
+        if response.status_code == 200:
+            try:
+                json_data = response.json()
+            except ValueError:
+                print(f"❌ Server did not return JSON: {response.text}")
+                json_data = {"error": "Invalid JSON response"}
+        else:
+            print(f"❌ Error {response.status_code}: {response.text}")
+            json_data = {"error": f"HTTP {response.status_code}"}
+
+        # print(f"📡 Response >> [{BWHTE}{REQUEST_FROM}{RES}] {json_data}")
+        return json_data, REQUEST_FROM
+
+    except Exception as e:
+        print(f"❌ Error calling API: {e}")
+        return {"error": str(e)}, REQUEST_FROM
+
+def extract_game_data(data: list) -> dict:
+    if data and isinstance(data, list) and len(data) > 0:
+        trending_games = [(game['name'], game['value']) for game in data if game['value'] >= 80]
+        return trending_games
+
+def pct(p):
+    if p is None:
+        return 0.0
+    if isinstance(p, str) and '%' in p:
+        return float(p.strip('%'))
+    try:
+        return float(p)
+    except (TypeError, ValueError):
+        return 0.0
+
+
+if __name__ == "__main__":
+    print(f"\n\n\t📘 {MAG}SCATTER TREND CHECK{RES}\n\n")
+    providers = list(PROVIDERS.items())
+    print(render_providers())
+        
+    while True:
+        try:
+            choice = int(input("\n\t🔔 Choose Provider: "))
+            if 1 <= choice <= len(providers):
+                provider = providers[choice - 1][0]
+                provider_name = providers[choice - 1][1].provider
+                provider_color = providers[choice - 1][1].color
+                print(f"\n\tSelected: {provider_color}{provider_name}{RES}")
+                break
+            else:
+                print("\t⚠️  Invalid choice. Try again.")
+        except ValueError:
+            print("\t⚠️  Please enter a valid number.")
+
+    data, request_from = get_game_data_from_local_api(provider)
+    if data and "success" in data:
+        parsed_data = extract_game_data(data.get('data'))
+        print(f'\n\t{DGRY}Checking Trend...{RES}\n')
+        for name, value in sorted(parsed_data, key=lambda g: g[0]):
+            # GET HELPSLOT METER TREND
+            fetch_data = fetch_jackpot(provider, name, session_id=1)
+            if pct(fetch_data.get('jackpot')) >= 90:
+                print(f"\t{YEL}{name}{RES} {DGRY}→ {RED}{value}{RES}")
+            else:
+                print(f"\t{BLRED}No Trending Games Found!{RES}")
+                break
